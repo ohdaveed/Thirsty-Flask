8ed70c1f267f702785151e8e954577660b249a52
David Arrizon <arrizon.david@gmail.com>
2019-11-20 00:52:18 -0600

    can delete countdown from db

--- resources/countdowns.py
+++ resources/countdowns.py
@@ -64,3 +64,41 @@ def get_one_countdown(id):
         'code': 200,
         'message': "registered users can access more about this"
         }), 200
+
+# update route
+# @countdowns.route('/<id>', methods=['PUT'])
+# def update_dog(id):
+#     payload = request.get.json()
+
+#     countdown = models.Countdown.get_by_id(id)
+
+#     if(countdown.name == 'Jesus'):
+
+#         countdown.name = payload['name'] if 'name' in payload else None
+
+#         countdown.save()
+
+#         countdown_dict = model_to_dict(countdown)
+
+#         return jsonify(data={"Will update"}, status={
+#             'code': 200,
+#             'message': 'Resource updated successfully'}), 200
+
+#     else:
+#         return jsonify(data="Forbidden", status={
+#             'code': 403,
+#             'message': 'I want this to show until jurgen has users set up'}), 403
+
+@countdowns.route('/<id>', methods=["Delete"])
+def delete_countdown(id):
+
+    countdown_to_delete = models.Countdown.get_by_id(id)
+
+    countdown_name = countdown_to_delete.name
+    countdown_to_delete.delete_instance()
+
+    return jsonify(data='Countdown delete successfully', status={"code": 200, "message": "{} deleted successfully".format(countdown_name)})
+
+
+    
+
