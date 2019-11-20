0b77d5a602f5b5ce7a00679fc1a0fc053bba402e
David Arrizon <arrizon.david@gmail.com>
2019-11-20 13:52:14 -0600

    removed test routes from code

--- app.py
+++ app.py
@@ -22,6 +22,21 @@ login_manager = LoginManager()
 
 login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None 
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return jsonify(data={
+        'error': 'User not logged in.'}, status={
+        'code': 401, 
+        'message': 'you must be loggesd into access that resource'
+        }), 401
+
 CORS(countdowns, origins=["http://localhost:3000"], supports_credentials=True)
 
 CORS(users, origins=["http://localhost:3000"], supports_credentials=True)
