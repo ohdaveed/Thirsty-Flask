af5751725d46a455ec4af7dfabc0b8943e89f7bc
David Arrizon <arrizon.david@gmail.com>
2019-11-19 19:03:48 -0600

    created models.py

---
+++ models.py
@@ -0,0 +1,31 @@
+import datetime
+
+from peewee import *
+
+from flask_login import UserMixin
+
+DATABASE = SqliteDatabase('countdowns.sqlite')
+
+class User(UserMixin, Model):
+    email = CharField(unique = True)
+    password = CharField()
+
+    class Meta:
+        database = DATABASE
+
+class Countdown(Model):
+    name = CharField()
+    owner = ForeignKey(User, backref='countdowns')
+    image = CharField()
+    timer = Integer
+    created_at = DateTimeField(default=datetime.datetime.now)
+    countdown_type = CharField()
+
+    class Meta:
+        database = DATABASE
+
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([User, Countdown], safe=True)
+    print("Created tables if they weren't already there")
+    DATABASE.close()
